48557cd11029aa3077405f1fac07ab4f88c91439
Matthew Janczak <50370491+jumanji1928@users.noreply.github.com>
2023-01-10 14:18:17 -0600

    Update tab.js

--- tab.js
+++ tab.js
@@ -10,6 +10,8 @@ function showhidetabs(tab){
   for(let i = 0; i < tab_ids.length; i++){
     if(tab_ids[i] == tab){
     document.getElementById(tab_ids[i]).classList.add("show");
+    }else {
+      document.getElementById(tab_ids[i]).classList.add("hide");
     }
   }
 }
