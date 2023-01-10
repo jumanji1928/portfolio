43ab001a801f58cb523e9be670ad15b84bc7c8e8
Matthew Janczak <50370491+jumanji1928@users.noreply.github.com>
2023-01-10 14:08:00 -0600

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,5 +1,7 @@
 const tab_ids = ["intro"];
 
 for (let i = 0; i < tab_ids.length; i++){
-  document.getElementById(tab_ids[i]).classList.add("hide");
+  if(tab_ids != "intro"){
+    document.getElementById(tab_ids[i]).classList.add("hide");
+  }
 }
