eec943afd9ab95674b4fd06a7f20c20793a66d16
Matthew Janczak <50370491+jumanji1928@users.noreply.github.com>
2023-01-10 14:32:29 -0600

    Update tab.js

--- tab.js
+++ tab.js
@@ -9,10 +9,8 @@ for (let i = 0; i < tab_ids.length; i++){
 function showhidetabs(tab){
   console.log(tab);
   for(let i = 0; i < tab_ids.length; i++){
+    document.getElementById(tab_ids[i]).classList.add("hide");
     if(tab_ids[i] == tab){
-    document.getElementById(tab_ids[i]).classList.add("show");
-    }else {
-      document.getElementById(tab_ids[i]).classList.add("hide");
-    }
+      document.getElementById(tab_ids[i]).classList.add("show");
   }
 }
