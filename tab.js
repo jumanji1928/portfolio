87956104da1fe887a1e02f01eac127d704184dd1
Matthew Janczak <50370491+jumanji1928@users.noreply.github.com>
2023-01-11 10:18:07 -0600

    Update tab.js

--- tab.js
+++ tab.js
@@ -2,7 +2,7 @@ const tab_ids = ["contact", "skills", "projects", "intro"];
 const tab_title = ["Contact", "Skills", "Projects", "Intro"];
 
 for(let i=0; i<tab_ids.length; i++){
-document.getElementById("navbar_buttons").insertAdjacentHTML("afterbegin",'<button class="btn btn-dark" type="button" onclick="showhidetabs(`'+ tab_ids[i] + '`)">'+ tab_ids[i]+'</button>');
+document.getElementById("navbar_buttons").insertAdjacentHTML("afterbegin",'<button class="btn btn-dark" type="button" onclick="showhidetabs(`'+ tab_ids[i] + '`)">'+ tab_title[i]+'</button>');
 }
 
 for (let i = 0; i < tab_ids.length; i++){
