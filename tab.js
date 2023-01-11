cdfb1b6f56f6e178e1f0358b1069fe5c8ae69fb4
Matthew Janczak <50370491+jumanji1928@users.noreply.github.com>
2023-01-11 13:30:56 -0600

    Update tab.js

--- tab.js
+++ tab.js
@@ -2,7 +2,7 @@ const tab_ids = ["contact", "skills", "projects", "intro"];
 const tab_title = ["Contact", "Skills", "Projects", "Intro"];
 
 for(let i=0; i<tab_ids.length; i++){
-document.getElementById("navbar_buttons").insertAdjacentHTML("afterbegin",'<li class="nav-item"><button class="nav-link btn" aria-current="page" onclick="showhidetabs(`'+ tab_ids[i] + '`)">'+ tab_title[i]+'</button></li>');
+document.getElementById("navbar_buttons").insertAdjacentHTML("afterbegin",'<li class="nav-item"><a class="nav-link" aria-current="page" onclick="showhidetabs(`'+ tab_ids[i] + '`)">'+ tab_title[i]+'</a></li>');
 }
 
 for (let i = 0; i < tab_ids.length; i++){
@@ -14,9 +14,9 @@ for (let i = 0; i < tab_ids.length; i++){
 function showhidetabs(tab){
   console.log(tab);
   for(let i = 0; i < tab_ids.length; i++){
-    document.getElementById(tab_ids[i]).classList.remove("show");
+    document.getElementById(tab_ids[i]).classList.remove("shows");
     if(tab_ids[i] == tab){
-      document.getElementById(tab_ids[i]).classList.add("show");
+      document.getElementById(tab_ids[i]).classList.add("shows");
     }else {
       document.getElementById(tab_ids[i]).classList.add("hide");
     }
